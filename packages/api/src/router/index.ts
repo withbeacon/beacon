a44d99adf89d8f096f453a0336f67ab3449cafdc
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-11-08 13:16:53 +0530

    chore(api/router): rm obsolete post router

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,10 +1,7 @@
 // src/server/router/index.ts
 import { t } from "../trpc";
 
-import { postRouter } from "./post";
-
 export const appRouter = t.router({
-  post: postRouter,
 });
 
 // export type definition of API
