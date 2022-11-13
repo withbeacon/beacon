49f01f5d4760c9458b75937f4507070726e32cff
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-11-13 12:19:14 +0530

    feat(web): implement /snippet page

---
+++ apps/web/src/pages/snippet.tsx
@@ -0,0 +1,124 @@
+import { Logo, Button } from "@spark/ui";
+import { ClipboardIcon, ClipboardCheckIcon } from "@spark/ui";
+import Link from "next/link";
+
+import { config, animated, useSpring } from "@react-spring/web";
+import { cx } from "class-variance-authority";
+import { trpc } from "~/utils";
+import { useState } from "react";
+
+export default function Onboard() {
+  const [copied, setCopied] = useState(false);
+  const idQuery = trpc.user.getId.useQuery();
+
+  const containerSprings = useSpring({
+    from: {
+      opacity: 0,
+      transform: "translateY(-10px)",
+    },
+
+    to: {
+      opacity: 1,
+      transform: "translateY(0px)",
+    },
+
+    config: config.slow,
+    delay: 100,
+  });
+
+  function handleCopy() {
+    navigator.clipboard.writeText(snippet);
+
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
+  }
+
+  if (idQuery.isLoading) {
+    return <Loading />;
+  }
+
+  let snippet = "";
+
+  if (typeof window !== "undefined") {
+    snippet = `<script
+  defer 
+  data-spark
+  data-id="${idQuery.data}"
+  src="${window.location.origin}/track.js"
+></script>`;
+  }
+
+  return (
+    <div className="h-screen w-screen grid place-items-center">
+      <animated.div
+        className="flex flex-col max-w-lg px-6 md:p-0 items-start gap-6"
+        style={{ ...containerSprings }}
+      >
+        <div className="flex items-center text-gray-800">
+          <Logo className="-mx-2 -my-4 w-16 h-16" />
+          <h2 className="text-2xl md:text-3xl font-bold">Spark</h2>
+        </div>
+
+        <h1 className="text-xl text-gray-600 font-semibold md:text-2xl">
+          Just copy and paste this snippet to any of your website{" "}
+          <span className="text-gray-800">and that&apos;s it!</span>
+        </h1>
+
+        <pre className="bg-gray-900 text-gray-100 w-full p-4 rounded-2xl font-mono relative selection:bg-primary-900 text-sm md:text-base">
+          <Button className="absolute right-4 top-4" onClick={handleCopy}>
+            {copied ? (
+              <ClipboardCheckIcon
+                className={cx(
+                  "w-5 h-5 md:w-6 w-6",
+                  copied ? "text-green-300" : "text-gray-100"
+                )}
+              />
+            ) : (
+              <ClipboardIcon
+                className={cx(
+                  "w-5 h-5 md:w-6 w-6",
+                  copied ? "text-green-300" : "text-gray-100"
+                )}
+              />
+            )}
+          </Button>
+          {snippet}
+        </pre>
+
+        <Link className="w-full" href="/">
+          <Button intent="primary" size="lg" filled fullWidth>
+            Done!
+          </Button>
+        </Link>
+      </animated.div>
+    </div>
+  );
+}
+
+function Loading() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center flex-col text-xl font-semibold gap-4 text-gray-800">
+      <svg
+        className="animate-spin h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+      <span>Just a second...</span>
+    </div>
+  );
+}
