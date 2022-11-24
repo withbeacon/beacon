0d977d632367460b70f9945e39569ab9d6e4b169
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-11-24 06:31:59 +0530

    feat(web/styles): implement hdie scrollbar utility class

--- apps/web/src/components/metrics/container.tsx
+++ apps/web/src/components/metrics/container.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export function MetricsContainer({ children }: PropsWithChildren) {
   return (
-    <div className="flex flex-col gap-4 px-5 py-4 rounded-xl bg-white text-gray-800 border border-gray-200 shadow-xl shadow-gray-100 md:w-1/2 w-full h-full">
+    <div className="relative flex flex-col gap-4 px-5 py-4 rounded-xl bg-white text-gray-800 border border-gray-200 shadow-xl shadow-gray-100 w-full h-96 overflow-y-scroll hide-scrollbar">
       {children}
     </div>
   );
