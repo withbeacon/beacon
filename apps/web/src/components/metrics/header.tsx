d479ee9a4fcda48037bc7c831480248982ff0e34
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-11-25 06:26:25 +0530

    fix(web/metrics): responsiveness on small devices

--- apps/web/src/components/metrics/header.tsx
+++ apps/web/src/components/metrics/header.tsx
@@ -1,6 +1,6 @@
 import { cx } from "class-variance-authority";
 
-interface Option {
+export interface Option {
   title: string;
   active: boolean;
   onClick: () => void;
@@ -13,22 +13,31 @@ interface Props {
 
 export function MetricsHeader({ title, options }: Props) {
   return (
-    <div className="flex items-center w-full">
-      <h2 className="text-lg md:text-xl font-semibold text-gray-800">{title}</h2>
-      <div className="ml-auto flex gap-4 text-base font-medium">
-        {options && options.map(({ active, title, onClick }) => (
-          <button
-            key={title}
-            onClick={() => onClick()}
-            className={cx(
-              active ? "text-gray-800" : "text-gray-600 font-normal",
-              "transition-colors duration-200 hover:text-gray-800 outline-none"
-            )}
-          >
-            {title}
-          </button>
-        ))}
-      </div>
+    <div
+      className={cx(
+        "flex items-center w-full",
+        options && "overflow-scroll hide-scrollbar"
+      )}
+    >
+      <h2 className="text-lg md:text-xl font-semibold text-gray-800">
+        {title}
+      </h2>
+      {options && (
+        <div className="ml-6 md:ml-auto flex gap-4 text-base font-medium">
+          {options.map(({ active, title, onClick }) => (
+            <button
+              key={title}
+              onClick={() => onClick()}
+              className={cx(
+                active ? "text-gray-800" : "text-gray-600 font-normal",
+                "transition-colors duration-200 hover:text-gray-800 outline-none"
+              )}
+            >
+              {title}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
