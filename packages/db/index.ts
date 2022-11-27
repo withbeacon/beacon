fc4d06246ae3a40af48d5723c7bcfaee36d39a3f
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-11-27 15:03:23 +0530

    perf: cache data in redis

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,4 +1,36 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import Redis from "ioredis";
+
+import { createPrismaRedisCache } from "prisma-redis-middleware";
+
+if (!process.env.REDIS_URL || !process.env.REDIS_PASSWORD) {
+  throw new Error("Missing REDIS_URL or REDIS_PASSWORD");
+}
+
+const redisClient = new Redis(
+  `redis://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_URL}`
+);
+
+const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache({
+  models: [
+    { model: "User", excludeMethods: ["findMany"] },
+    { model: "Post", cacheTime: 180, cacheKey: "article" },
+  ],
+  storage: {
+    type: "redis",
+    options: {
+      client: redisClient,
+      invalidation: { referencesTTL: 300 },
+      log: console,
+    },
+  },
+  cacheTime: 300,
+  excludeModels: ["User", "Account", "Session"],
+  excludeMethods: ["count", "groupBy"],
+  onError: (key) => console.error("[redis] error: ", key),
+  onHit: (key) => console.info("[redis] hit ", key),
+  onMiss: (key) => console.info("[redis] miss: ", key)
+});
 
 declare global {
   // allow global `var` declarations
@@ -15,6 +47,9 @@ export const prisma =
         : ["error"],
   });
 
+prisma.$use(cacheMiddleware);
+
 if (process.env.NODE_ENV !== "production") {
   global.prisma = prisma;
 }
+
