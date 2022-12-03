1e4331be42ef2f24eb8efe8f8e2d914e26a346f9
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-03 06:56:18 +0530

    revert(config): use inter instead of mona sans

--- packages/config/tailwind.config.cjs
+++ packages/config/tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Mona Sans", "sans-serif"],
+        sans: ["Inter", "sans-serif"],
         mono: ['"Jetbrains Mono"', "monospace"],
       },
       colors: {
