677b4fdb748907f5ac86c9d067a377b43f133466
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-03 16:07:42 +0530

    feat(web/noWebsite): implement dark mode

--- apps/web/src/components/noWebsite.tsx
+++ apps/web/src/components/noWebsite.tsx
@@ -32,18 +32,18 @@ export function NoWebsite() {
   }
 
   return (
-    <div className="grid h-full w-full place-items-center overflow-hidden py-6 md:py-0">
-      <div className="flex max-w-lg flex-col items-center gap-6 px-6 text-center text-gray-800 md:p-0">
+    <div className="grid h-full w-full place-items-center overflow-hidden bg-gray-100 py-6 dark:bg-gray-900 md:py-0">
+      <div className="flex max-w-lg flex-col items-center gap-6 px-6 text-center text-gray-800 dark:text-gray-200 md:p-0">
         <BarIcon className="h-16 w-16" />
         <h1 className="text-xl font-semibold md:text-2xl">
           Looks like you have not linked any of your website yet..
         </h1>
-        <p className="space-lg text-sm text-gray-700 md:text-base">
+        <p className="space-lg text-sm text-gray-700 dark:text-gray-300 md:text-base">
           No worries, you can connect your website with bud by just embedding
           the script below in your website.
         </p>
 
-        <div className="relative rounded-2xl bg-gray-900 p-4 text-left font-mono text-sm text-gray-100 selection:bg-primary-900 md:w-full md:text-base">
+        <div className="relative rounded-2xl bg-gray-900 p-4 text-left font-mono text-sm text-gray-100 selection:bg-primary-900 md:w-full md:text-base dark:border dark:border-gray-800">
           <Button className="absolute right-4 top-4" onClick={handleCopy}>
             {copied ? (
               <ClipboardCheckIcon
