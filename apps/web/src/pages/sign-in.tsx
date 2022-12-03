41ed02a99f542d76c8b3cb64b1bab5605bc99780
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-03 16:08:00 +0530

    feat(web/sign-in): implement dark mode

--- apps/web/src/pages/sign-in.tsx
+++ apps/web/src/pages/sign-in.tsx
@@ -29,19 +29,21 @@ export default function SignIn() {
   }
 
   return (
-    <div className="grid h-screen w-screen place-items-center">
+    <div className="grid h-screen w-screen place-items-center bg-gray-100 dark:bg-gray-900">
       <animated.div
         className="flex max-w-lg flex-col items-start gap-6 px-6 md:p-0"
         style={{ ...containerSprings }}
       >
-        <div className="flex items-center text-gray-800">
+        <div className="flex items-center text-gray-800 dark:text-gray-200">
           <Logo className="mr-2 h-12 w-12" />
           <h2 className="text-2xl font-bold md:text-3xl">Bud</h2>
         </div>
 
-        <h1 className="text-xl font-semibold text-gray-600 md:text-2xl">
+        <h1 className="text-xl font-medium text-gray-600 dark:text-gray-400 md:text-2xl">
           Analytics should be privacy friendly, fast and simple.{" "}
-          <span className="text-gray-800">That&apos;s what we do.</span>
+          <span className="text-gray-800 dark:text-gray-200">
+            That&apos;s what we do.
+          </span>
         </h1>
 
         <Button
@@ -54,7 +56,7 @@ export default function SignIn() {
         >
           <GoogleIcon /> Sign In
         </Button>
-        <p className="text-sm text-gray-500 md:text-base">
+        <p className="text-sm text-gray-500 dark:text-gray-400 md:text-base">
           By signing in to Bud, you agree to our terms of service and privacy
           policy.
         </p>
