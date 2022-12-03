37d9174df2ac7502a9344293cbba0ed5223acde2
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-03 16:08:20 +0530

    feat(loading): implement dark mode

--- packages/ui/src/loading.tsx
+++ packages/ui/src/loading.tsx
@@ -2,9 +2,9 @@ import { Logo } from "./logo";
 
 export function Loading() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 text-xl font-semibold text-gray-800">
-      <div className="flex items-center justify-center fill-gray-900 text-gray-900">
-        <Logo className="h-12 w-12 mr-2" />
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 text-xl font-semibold dark:bg-gray-900">
+      <div className="flex items-center justify-center text-gray-900 dark:text-gray-100">
+        <Logo className="h-10 w-10 mr-2" />
         <span className="text-3xl font-bold">Bud</span>
       </div>
     </div>
