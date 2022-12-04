f89ac08112d41b6e8d46b6a1a60902ed3aa08588
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-04 12:27:37 +0530

    fix(web/container): decrease padding in small devices

--- apps/web/src/components/metrics/container.tsx
+++ apps/web/src/components/metrics/container.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export function MetricsContainer({ children }: PropsWithChildren) {
   return (
-    <div className="hide-scrollbar flex h-96 w-full flex-col gap-4 overflow-y-scroll rounded-xl p-4 text-gray-800 dark:text-gray-200 bg-transparent">
+    <div className="hide-scrollbar flex h-96 w-full flex-col gap-4 overflow-y-scroll rounded-xl p-2 md:p-4 text-gray-800 dark:text-gray-200 bg-transparent">
       {children}
     </div>
   );
