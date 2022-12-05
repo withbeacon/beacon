64fa8ecbf7bf50cc4d7f955df1b650559ed19cc2
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-05 15:44:48 +0530

    test(ui/button): implement storybook tests

---
+++ packages/ui/src/button.stories.tsx
@@ -0,0 +1,61 @@
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Button } from "./button";
+
+import { within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
+const children = "Button";
+const size = "md";
+
+export default {
+  title: "Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  intent: "primary",
+  filled: true,
+  children,
+  size,
+};
+
+Primary.play = async ({ canvasElement }) => {
+  const button = within(canvasElement).getByText(children);
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveTextContent(children);
+};
+
+export const Normal = Template.bind({});
+
+Normal.args = {
+  intent: "normal",
+  filled: true,
+  children,
+  size,
+};
+
+Normal.play = async ({ canvasElement }) => {
+  const button = within(canvasElement).getByText(children);
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveTextContent(children);
+};
+
+export const Destructive = Template.bind({});
+
+Destructive.args = {
+  intent: "destructive",
+  filled: true,
+  children,
+  size,
+};
+
+Destructive.play = async ({ canvasElement }) => {
+  const button = within(canvasElement).getByText(children);
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveTextContent(children);
+};
+
