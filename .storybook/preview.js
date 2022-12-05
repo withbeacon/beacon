9af389fca23c41ea1f2d3268e2a31f740d486a89
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-05 16:50:52 +0530

    fix(storybook): next/image not working

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import "./tailwind.css";
+import * as nextImage from "next/image"
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,3 +10,31 @@ export const parameters = {
     },
   },
 }
+
+Object.defineProperty(nextImage, "default", {
+  configurable: true,
+  value: props => {
+    const { width, height } = props
+    const ratio = (height / width) * 100
+    return (
+      <div
+        style={{
+          paddingBottom: `${ratio}%`,
+          position: "relative",
+        }}
+      >
+        <img
+          style={{
+            objectFit: "cover",
+            position: "absolute",
+            minWidth: "100%",
+            minHeight: "100%",
+            maxWidth: "100%",
+            maxHeight: "100%",
+          }}
+          {...props}
+        />
+      </div>
+    )
+  },
+})
