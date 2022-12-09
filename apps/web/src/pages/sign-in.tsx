4820f772da2bbc5cc8f85bb66cf7ef39deec1524
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-09 20:15:55 +0530

    feat(web/sign-in): optimize seo title

--- apps/web/src/pages/sign-in.tsx
+++ apps/web/src/pages/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Logo, Button } from "@bud/ui";
 import { GoogleIcon } from "@bud/ui";
+import Head from "next/head";
 
 import { config, animated, useSpring } from "@react-spring/web";
 import { useState } from "react";
@@ -29,38 +30,44 @@ export default function SignIn() {
   }
 
   return (
-    <div className="grid h-screen w-screen place-items-center bg-gray-100 dark:bg-gray-900">
-      <animated.div
-        className="flex max-w-lg flex-col items-start gap-6 px-6 md:p-0"
-        style={{ ...containerSprings }}
-      >
-        <div className="flex items-center text-gray-800 dark:text-gray-200">
-          <Logo className="mr-2 h-12 w-12" />
-          <h2 className="text-2xl font-bold md:text-3xl">Bud</h2>
-        </div>
+    <>
+      <Head>
+        <title>Sign In to Bud</title>
+      </Head>
 
-        <h1 className="text-xl font-medium text-gray-600 dark:text-gray-400 md:text-2xl">
-          Analytics should be privacy friendly, fast and simple.{" "}
-          <span className="text-gray-800 dark:text-gray-200">
-            That&apos;s what we do.
-          </span>
-        </h1>
-
-        <Button
-          intent="primary"
-          size="lg"
-          onClick={handleSignIn}
-          loading={loading}
-          filled
-          fullWidth
+      <div className="grid h-screen w-screen place-items-center bg-gray-100 dark:bg-gray-900">
+        <animated.div
+          className="flex max-w-lg flex-col items-start gap-6 px-6 md:p-0"
+          style={{ ...containerSprings }}
         >
-          <GoogleIcon /> Sign In
-        </Button>
-        <p className="text-sm text-gray-500 dark:text-gray-400 md:text-base">
-          By signing in to Bud, you agree to our terms of service and privacy
-          policy.
-        </p>
-      </animated.div>
-    </div>
+          <div className="flex items-center text-gray-800 dark:text-gray-200">
+            <Logo className="mr-2 h-12 w-12" />
+            <h2 className="text-2xl font-bold md:text-3xl">Bud</h2>
+          </div>
+
+          <h1 className="text-xl font-medium text-gray-600 dark:text-gray-400 md:text-2xl">
+            Analytics should be privacy friendly, fast and simple.{" "}
+            <span className="text-gray-800 dark:text-gray-200">
+              That&apos;s what we do.
+            </span>
+          </h1>
+
+          <Button
+            intent="primary"
+            size="lg"
+            onClick={handleSignIn}
+            loading={loading}
+            filled
+            fullWidth
+          >
+            <GoogleIcon /> Sign In
+          </Button>
+          <p className="text-sm text-gray-500 dark:text-gray-400 md:text-base">
+            By signing in to Bud, you agree to our terms of service and privacy
+            policy.
+          </p>
+        </animated.div>
+      </div>
+    </>
   );
 }
