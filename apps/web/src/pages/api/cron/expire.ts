cb637d71f2756b7500494a8a235abb4de7945bd0
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-11 18:26:11 +0530

    feat(web/api): implement cron job to check and expire sessions

---
+++ apps/web/src/pages/api/cron/expire.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@bud/db";
+import { fromNow } from "@bud/basics";
+import { env } from "~/env/server.mjs";
+import cuid from "cuid";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Invalid method" });
+    return;
+  }
+
+  const { APP_KEY } = env;
+  const ACTION_KEY = req.headers.authorization?.split(" ")[1];
+
+  if (ACTION_KEY !== APP_KEY) {
+    res.status(401).json({ error: "Invalid authorization" });
+    return;
+  }
+
+  const sessions = await prisma.userSession.findMany({
+    where: {
+      expires: {
+        lte: new Date(+(new Date()) - 48 * 3600 * 1000),
+      },
+    },
+  });
+
+  const ids = sessions.map(({ id }) => id);
+
+  for (let id of ids) {
+    await prisma.userSession.update({
+      where: {
+        id
+      },
+      data: {
+        id: cuid(),
+      }
+    })
+  }
+
+  return res.send("OK");
+}
