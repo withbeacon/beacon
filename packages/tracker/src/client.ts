ea3012ff2c04b94da42c172c892c8bf7e482ddb2
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-15 16:52:06 +0530

    feat(tracker): rm obsolete data-website-name and data-id vars

--- packages/tracker/src/client.ts
+++ packages/tracker/src/client.ts
@@ -1,7 +1,7 @@
 export const { href: url } = window.location;
 export const { width, height } = window.screen;
 export const { userAgent, doNotTrack } = navigator;
-export const { referrer } = document;
+export const { title, referrer } = document;
 export const screen = `${width}x${height}`;
 export const isDoNotTrackEnabled = doNotTrack === '1';
 
@@ -11,5 +11,3 @@ if (!script) {
   throw new Error("[bud] unable to get the script for bud analytics")
 }
 
-export const id = script.getAttribute("data-id");
-export const title = script.getAttribute("data-website-name") ?? document.title;
