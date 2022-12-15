6e4dc931161d0df637fa39ee8a4d9292f7d3194a
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-15 16:56:47 +0530

    fix(web/nav): show website select only in /{dashboard|share}/[id]

--- apps/web/src/components/nav.tsx
+++ apps/web/src/components/nav.tsx
@@ -13,57 +13,65 @@ interface Props {
   shared?: boolean;
 }
 
-export function Nav({ shared = false }: Props) {
+function Website() {
   const router = useRouter();
-  const { data } = useSession();
   const { id } = router.query;
   const [date] = useDate();
+
   const query = trpc.website.get.useQuery({
     id: id as string,
     from: date.from,
     to: date.to,
   });
 
+  return (
+    <>
+      {query.data && (
+        <div className="ml-2 flex items-center gap-2 text-gray-800 dark:text-gray-200">
+          <div className="hidden cursor-pointer items-center gap-2 md:flex">
+            <span className="mr-2 text-gray-500 dark:text-gray-700">{"/"}</span>
+            <img
+              src={query.data.favicon || ""}
+              alt={query.data.name}
+              className="h-5 w-5"
+            />
+            <h2>{query.data.name}</h2>
+            <WebsiteSelect>
+              <SelectIcon className="-ml-1 h-5 w-5" />
+            </WebsiteSelect>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export function Nav({ shared = false }: Props) {
+  const { data } = useSession();
+  const { push, query } = useRouter();
+
   const baseIconStyles =
     "w-6 h-6 hover:opacity-90 cursor-pointer text-gray-800 dark:text-gray-200";
-
-  if (query.isLoading) return <></>;
 
   return (
     <nav className="mx-6 flex items-center justify-between border-b border-gray-200 py-4 dark:border-gray-800 dark:bg-gray-900">
       <div className="flex items-center gap-2 text-xl font-medium text-gray-800 dark:text-gray-200">
         <Logo className="h-6 w-6" />
         <span>Bud</span>
-
-        {query.data && (
-          <div className="ml-2 flex items-center gap-2 text-gray-800 dark:text-gray-200">
-            <div className="hidden cursor-pointer items-center gap-2 md:flex">
-              <span className="mr-2 text-gray-500 dark:text-gray-700">
-                {"/"}
-              </span>
-              <img
-                src={query.data.favicon || ""}
-                alt={query.data.name}
-                className="h-5 w-5"
-              />
-              <h2>{query.data.name}</h2>
-              <WebsiteSelect>
-                <SelectIcon className="-ml-1 h-5 w-5" />
-              </WebsiteSelect>
-            </div>
-          </div>
-        )}
+        <Website />
       </div>
 
       <div className="hidden items-center gap-4 md:flex">
-        <DateSelect />
+        {query.id && (
+          <DateSelect />
+        )}
         <SearchIcon aria-label="Search" className={baseIconStyles} />
         <Feedback>
           <HelpIcon aria-label="Help" className={baseIconStyles} />
         </Feedback>
         {shared && (
           <Button
-            onClick={() => router.push("/sign-in")}
+            onClick={() => push("/sign-in")}
             intent="primary"
             size="sm"
             className="!px-6"
