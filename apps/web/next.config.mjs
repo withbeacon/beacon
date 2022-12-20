125f9dc82a82588ca49c3b5af7de59c996a1739e
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-20 15:56:46 +0530

    chore(web): replace next-tranpsile-modules package w/ nextjs transpilePackages

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
 import { env } from "./src/env/server.mjs";
-import withTM from "next-transpile-modules";
 
 /**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
  * @template {import('next').NextConfig} T
  * @param {T} config - A generic parameter that flows through to the return type
  * @constraint {{import('next').NextConfig}}
@@ -14,9 +10,10 @@ function defineNextConfig(config) {
   return config;
 }
 
-export default withTM(["@bud/api", "@bud/db", "@bud/ui", "@bud/auth", "@bud/basics"])(
-  defineNextConfig({
-    reactStrictMode: true,
-    swcMinify: true,
-  })
-);
+export default defineNextConfig({
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    transpilePackages: ["@bud/api", "@bud/db", "@bud/ui", "@bud/auth", "@bud/basics"],
+  }
+});
