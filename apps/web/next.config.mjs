17c9c93aac4f6ec0666844a80cb45bbc1a0da4d6
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-24 12:22:08 +0530

    feat(web): replace next-transpile-modules with next tranpsilePackages

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,21 +2,24 @@
 import { env } from "./src/env/server.mjs";
 import withTM from "next-transpile-modules";
 
-/**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
-function defineNextConfig(config) {
-  return config;
-}
-
-export default withTM(["@bud/api", "@bud/db", "@bud/ui", "@bud/auth", "@bud/basics"])(
-  defineNextConfig({
-    reactStrictMode: true,
-    swcMinify: true,
-  })
-);
+export default {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    appDir: true,
+    transpilePackages: [
+      "@bud/api",
+      "@bud/db",
+      "@bud/ui",
+      "@bud/auth",
+      "@bud/basics",
+    ],
+    serverComponentsExternalPackages: [
+      "prisma",
+      "@prisma/client",
+      "postcss",
+      "tailwindcss",
+      "autoprefixer",
+    ],
+  },
+};
