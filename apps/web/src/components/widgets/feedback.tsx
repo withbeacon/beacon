ddaf2509ca68028985d109e747c40658c9467d35
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-25 22:41:56 +0530

    refactor(feedback): add use client

--- apps/web/src/components/widgets/feedback.tsx
+++ apps/web/src/components/widgets/feedback.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import type { PropsWithChildren } from "react";
 import { FeedbackFish } from "@feedback-fish/react";
 
 import { env } from "~/env/client.mjs";
 
-export function Feedback({ children }: PropsWithChildren) {
+export default function Feedback({ children }: PropsWithChildren) {
   return (
     <FeedbackFish projectId={env.NEXT_PUBLIC_FEEDBACK_FISH}>
       <span>{children}</span>
