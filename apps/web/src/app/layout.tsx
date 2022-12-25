dc08cd429658adda06fa0019bd39affe8ae4f5c4
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-25 22:30:11 +0530

    perf(web): use @next/font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,17 +1,17 @@
 import type { PropsWithChildren } from "react";
 import { ServerThemeProvider } from "@wits/next-themes";
+import { Inter } from "@next/font/google";
 
 import "~/styles/globals.css";
-import "~/styles/fonts.css";
+
+const font = Inter({ subsets: ["latin"] });
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     <ServerThemeProvider>
-      <html>
+      <html lang="en" className={font.className}>
         <head />
-        <body>
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
     </ServerThemeProvider>
   );
