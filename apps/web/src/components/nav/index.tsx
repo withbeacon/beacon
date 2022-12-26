2e30dc892312e819783bdb4cbc5fdcb6ac70dbd4
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-26 11:13:38 +0530

    feat(web/nav): implement hideDateSelect & hideWebsiteSelect props

--- apps/web/src/components/nav/index.tsx
+++ apps/web/src/components/nav/index.tsx
@@ -1,8 +1,7 @@
 import { SearchIcon, HelpIcon, MenuIcon } from "@bud/ui";
 import { Logo } from "@bud/ui";
 import { Suspense } from "react";
-import DateSelect from "~/components/dateSelect";
-import Website from "./website";
+import Link from "next/link";
 
 import dynamic from "next/dynamic";
 
@@ -22,24 +21,40 @@ const SignInButton = dynamic(() => import("./signInButton"), {
   suspense: true
 });
 
+const DateSelect = dynamic(() => import("~/components/dateSelect"), {
+  suspense: true
+});
+
+const Website = dynamic(() => import("./website"), {
+  suspense: true
+});
+
 interface Props {
   loggedIn?: boolean;
+  hideDateSelect?: boolean;
+  hideWebsiteSelect?: boolean;
 }
 
-export default function Nav({ loggedIn = false }: Props) {
+export default function Nav({ loggedIn = false, hideDateSelect = false, hideWebsiteSelect = false }: Props) {
   const baseIconStyles =
     "w-6 h-6 hover:opacity-90 cursor-pointer text-gray-800 dark:text-gray-200";
 
   return (
     <nav className="mx-6 flex items-center justify-between border-b border-gray-200 py-4 dark:border-gray-800 dark:bg-gray-900">
       <div className="flex items-center gap-2 text-xl font-medium text-gray-800 dark:text-gray-200">
-        <Logo className="h-6 w-6" />
-        <span>Bud</span>
-        <Website />
+        <Link href="/" className="cursor-pointer flex items-center gap-2">
+          <Logo className="h-6 w-6" />
+          <span>Bud</span>
+        </Link>
+        <Suspense fallback={<p>Loading ...</p>}>
+          {!hideWebsiteSelect && <Website />}
+        </Suspense>
       </div>
 
       <div className="hidden items-center gap-4 md:flex">
-        <DateSelect />
+        <Suspense fallback={<p>Loading ...</p>}>
+          {!hideDateSelect && <DateSelect />}
+        </Suspense>
         {loggedIn ? (
           <>
             <SearchIcon aria-label="Search" className={baseIconStyles} />
