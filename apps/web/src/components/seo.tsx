c55f362f92018c4e18526af52a5ae69810ec0557
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-12-29 21:32:17 +0530

    fix(web/seo): rm next/head

--- apps/web/src/components/seo.tsx
+++ apps/web/src/components/seo.tsx
@@ -1,21 +1,22 @@
-import Head from "next/head";
-
 export default function DefaultSeo() {
   const opts = {
-    title: "Bud – fast, simple and privacy friendly analytics that you will love using.",
-    description: "Bud is a fast, simple and privacy friendly analytics tool that you will love using.",
-    socialImg: "https://github.com/vedantnn71/bud/raw/main/apps/web/public/social.png",
+    title:
+      "Bud – fast, simple and privacy friendly analytics that you will love using.",
+    description:
+      "Bud is a fast, simple and privacy friendly analytics tool that you will love using.",
+    socialImg:
+      "https://github.com/vedantnn71/bud/raw/main/apps/web/public/social.png",
     url: "https://github.com/vedantnn71/bud",
     favicon: "/favicon.ico",
     handle: "@usebud",
-  }
+  };
 
   return (
-    <Head>
+    <>
       <title>{opts.title}</title>
       <meta name="description" content={opts.description} />
-      <link rel="icon" href={opts.favicon} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href={opts.favicon} />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content={opts.handle} />
@@ -23,13 +24,12 @@ export default function DefaultSeo() {
       <meta name="twitter:title" content={opts.title} />
       <meta name="twitter:description" content={opts.description} />
       <meta name="twitter:image" content={opts.socialImg} />
-      
+
       <meta property="og:url" content={opts.url} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={opts.title }/>
+      <meta property="og:title" content={opts.title} />
       <meta property="og:description" content={opts.description} />
       <meta property="og:image" content={opts.socialImg} />
-    </Head>
-  )
+    </>
+  );
 }
-
