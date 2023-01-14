18117987ae06825999683a5f0a7ec8fd05be68c3
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-14 08:56:51 +0530

    fix(web/insights): low contrast border in light mode

--- apps/web/src/components/insights/index.tsx
+++ apps/web/src/components/insights/index.tsx
@@ -10,7 +10,7 @@ export interface InsightCardProps {
 
 function InsightCard({ data, label, value, timeFormat }: InsightCardProps) {
   return (
-    <div className="flex h-48 w-full flex-col gap-8 rounded-xl border border-gray-100 pt-6 dark:border-gray-800 overflow-hidden">
+    <div className="flex h-48 w-full flex-col gap-8 rounded-xl border border-gray-200 pt-6 dark:border-gray-800 overflow-hidden">
       <div className="flex justify-between px-8">
         <div className="flex flex-col">
           <span className="text-base text-gray-600 dark:text-gray-400">
