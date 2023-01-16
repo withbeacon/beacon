da0e984493586248ad56ae138729d8fcc0567788
Vedant Nandwana <70624701+vedantnn71@users.noreply.github.com>
2023-01-16 23:38:39 +0530

    fix(api/collect): don't process bot requests (#36)
    * refactor(api/collect): rm obsolete imports & functions
    
    * fix(api/collect): don't process bot requests

--- apps/web/src/pages/api/collect.ts
+++ apps/web/src/pages/api/collect.ts
@@ -1,19 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Prisma } from "@prisma/client";
-import Hashids from "hashids";
 
 import { prisma } from "@beacon/db";
 import { isExpired } from "@beacon/basics";
 import { getSession, parseAgent } from "~/utils";
-import { getServerSession } from "@beacon/auth";
-import { load } from "cheerio";
+import isBot from "isbot";
 import cuid from "cuid";
 
 type BodyParams = {
   url: string;
-  visitTime: number; // in milliseconds
-  screen: string; // screen size (width x height)
-  device: string; // mobile or desktop
+  visitTime: number;
+  screen: string;
+  device: string;
   userAgent: string;
   referrer: string;
   title: string;
@@ -22,8 +19,6 @@ type BodyParams = {
 
 type Events = Record<string, Record<string, boolean>>;
 type QueryParams = Record<string, string>;
-
-const hashid = new Hashids("beacon", 8);
 
 export default async function handler(
   req: NextApiRequest,
@@ -37,6 +32,11 @@ export default async function handler(
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST");
   res.setHeader("Accept", "application/json");
+
+  if (isBot(req.headers["user-agent"])) {
+    res.send("Ok");
+    return;
+  }
 
   const {
     url: href,
@@ -167,36 +167,3 @@ export default async function handler(
   return;
 }
 
-async function getPageDetails(
-  url: string,
-  userName: string
-): Promise<{ name: string; favicon: string | null }> {
-  const resp = await fetch(url);
-  const html = await resp.text();
-  const $ = load(html);
-
-  let favicon = $("link[rel='icon']").attr("href") || null;
-  const title = $("title").text();
-
-  // remove all the seperators
-  let [name] = title.split(/[-|:|−|–]/);
-
-  if (!name) {
-    name = `${userName}'s website`;
-  }
-
-  name = name.trim();
-  favicon = favicon ? new URL(favicon, url).href : null;
-  favicon = favicon ? await imageUrl(favicon) : null;
-
-  return { name, favicon };
-}
-
-async function imageUrl(url: string) {
-  const resp = await fetch(url);
-  const blob = await resp.blob();
-  const buffer = await blob.arrayBuffer();
-  const base64 = Buffer.from(buffer).toString("base64");
-
-  return `data:${blob.type};base64,${base64}`;
-}
