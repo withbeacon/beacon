35776dcda4fae13aa65c76ac9c95fe0f6297789f
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-20 14:03:08 +0530

    feat(web/websiteCard): implement loading shimmer ui

--- apps/web/src/components/websiteCard/index.tsx
+++ apps/web/src/components/websiteCard/index.tsx
@@ -8,6 +8,19 @@ interface Props {
   views: number;
 }
 
+export function WebsiteCardShimmer() {
+  return (
+    <div className="group flex flex-col items-start justify-start gap-4">
+      <div className="relative grid h-40 w-full place-items-center overflow-hidden rounded-2xl bg-gray-100 dark:bg-gray-800 animate-pulse" />
+
+      <div className="flex w-full justify-between animate-pulse flex-col gap-2">
+        <div className="bg-gray-300 dark:bg-gray-700 h-4 w-20"/>
+        <div className="text-gray-300 dark:bg-gray-700 w-28 h-3" />
+      </div>
+    </div>
+  );
+}
+
 export default function WebsiteCard({ url, name, favicon, views }: Props) {
   return (
     <div className="group flex flex-col items-start justify-start gap-4">
