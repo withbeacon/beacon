c48a9ee6cd02e17bb77ba46f40d9aa414e63a971
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-21 21:15:02 +0530

    feat(icon): write unit tests

---
+++ apps/icon/src/index.test.ts
@@ -0,0 +1,22 @@
+import { unstable_dev } from "wrangler";
+import type { UnstableDevWorker } from "wrangler";
+import { describe, expect, it, beforeAll, afterAll } from "vitest";
+
+describe("Worker", () => {
+  let worker: UnstableDevWorker;
+
+  beforeAll(async () => {
+    worker = await unstable_dev("src/index.ts", {
+      experimental: { disableExperimentalWarning: true },
+    });
+  });
+
+  afterAll(async () => {
+    await worker.stop();
+  });
+
+  it("should have content-type of image/x-icon", async () => {
+    const resp = await worker.fetch("/google.com");
+    expect(resp.headers.get("content-type")).toBe("image/x-icon");
+  });
+});
