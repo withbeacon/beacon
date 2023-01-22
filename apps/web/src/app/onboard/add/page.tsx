359d514e375f3425d53cadf2e9d9637278afc68a
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-22 12:28:36 +0530

    feat(app/onboard): implement /onboard/add

---
+++ apps/web/src/app/onboard/add/page.tsx
@@ -0,0 +1,17 @@
+import Form from "./form";
+
+import { protect } from "~/utils/auth";
+
+export default async function Page() {
+  await protect();
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold">Add your website</h1>
+      <p className="text-gray-500 dark:text-gray-400 mt-1">
+        Just tell us about the name and url of your website, we will handle the rest.
+      </p>
+      <Form />
+    </>
+  );
+}
