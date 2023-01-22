2e2a1fc802c9075b25f94d9b2919ca33659d16fb
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-22 09:11:50 +0530

    refactor(web/store): rm obsolete website

--- apps/web/src/store/website.ts
+++
@@ -1,5 +0,0 @@
-import { useAtom } from "jotai";
-import { persistedAtom } from "./utils";
-
-export const websiteAtom = persistedAtom<string | null>("website-id", null);
-export const useWebsite = () => useAtom(websiteAtom);
