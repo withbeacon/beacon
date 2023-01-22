e1d68d2ac18b9907c698e1e28117ce6915545a99
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-22 21:20:45 +0530

    feat(web/header): implement fallback loading ui

--- apps/web/src/components/header/index.tsx
+++ apps/web/src/components/header/index.tsx
@@ -8,3 +8,15 @@ export default function Header() {
     </header>
   );
 }
+
+export function HeaderShimmer() {
+  return (
+    <header className="flex w-full flex-col items-start justify-between gap-4 lg:flex-row lg:items-center lg:gap-0">
+      <div className="w-32 h-7 bg-gray-400 dark:bg-gray-600 animate-pulse" />
+      <div className="flex gap-2">
+        <div className="w-7 rounded-full h-7 bg-gray-400 dark:bg-gray-600 animate-pulse" />
+        <div className="w-32 h-7 bg-gray-400 dark:bg-gray-600 animate-pulse" />
+      </div>
+    </header>
+  );
+}
