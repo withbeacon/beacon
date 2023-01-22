8db1969dc49c46eec447054c1c82a2dbafd38573
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-22 12:27:35 +0530

    feat(app/onboard): implement onboarding layout

---
+++ apps/web/src/app/onboard/layout.tsx
@@ -0,0 +1,15 @@
+import type { PropsWithChildren } from "react";
+import { Logo } from "@beacon/ui";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className="flex min-h-screen flex-col items-center bg-white dark:bg-gray-900">
+      <header className="flex items-center justify-center py-6">
+        <Logo className="h-8 w-8" />
+        <h1 className="ml-2 text-2xl font-bold">Beacon</h1>
+      </header>
+
+      <main className="flex flex-col my-auto w-full gap-4 px-6 lg:px-0 lg:w-[32rem]">{children}</main>
+    </div>
+  );
+}
