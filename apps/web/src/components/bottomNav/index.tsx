48122d5e5b903ceda4f8b1a2502c5313b52c18a6
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-22 21:20:33 +0530

    feat(web/bottom-nav): implement fallback loading ui

--- apps/web/src/components/bottomNav/index.tsx
+++ apps/web/src/components/bottomNav/index.tsx
@@ -52,3 +52,20 @@ export default function BottomNav() {
     </nav>
   );
 }
+
+export function BottomNavShimmer() {
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-center gap-6 border-t border-gray-200 bg-gray-900 py-4 shadow-lg shadow-gray-900/10 dark:border-gray-800 dark:shadow-gray-900/10 lg:hidden z-40">
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-7 h-7 rounded-full bg-gray-400 dark:bg-gray-600 animate-pulse" />
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-7 h-7 rounded-full bg-gray-400 dark:bg-gray-600 animate-pulse" />
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-7 h-7 rounded-full bg-gray-400 dark:bg-gray-600 animate-pulse" />
+      </div>
+    </nav>
+  );
+}
+
