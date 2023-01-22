fdc9e5b3efde7c30cf63dea3588456101db08fae
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-22 09:10:50 +0530

    fix(web/bottom-nav): z index

--- apps/web/src/components/bottomNav/index.tsx
+++ apps/web/src/components/bottomNav/index.tsx
@@ -37,7 +37,7 @@ export default function BottomNav() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-center gap-6 border-t border-gray-200 bg-gray-900 py-4 shadow-lg shadow-gray-900/10 dark:border-gray-800 dark:shadow-gray-900/10 lg:hidden">
+    <nav className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-center gap-6 border-t border-gray-200 bg-gray-900 py-4 shadow-lg shadow-gray-900/10 dark:border-gray-800 dark:shadow-gray-900/10 lg:hidden z-40">
       <NavItem href="/">
         <HomeIcon />
       </NavItem>
