a83778237beb83d8654d008b9367c0f17d87a1ff
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-23 07:24:56 +0530

    feat(web/header): show title

--- apps/web/src/components/header/index.tsx
+++ apps/web/src/components/header/index.tsx
@@ -12,7 +12,7 @@ export default function Header() {
 export function HeaderShimmer() {
   return (
     <header className="flex w-full flex-col items-start justify-between gap-4 lg:flex-row lg:items-center lg:gap-0">
-      <div className="w-32 h-7 bg-gray-400 dark:bg-gray-600 animate-pulse" />
+      <h1 className="text-xl font-semibold">My Sites</h1>
       <div className="flex gap-2">
         <div className="w-7 rounded-full h-7 bg-gray-400 dark:bg-gray-600 animate-pulse" />
         <div className="w-32 h-7 bg-gray-400 dark:bg-gray-600 animate-pulse" />
