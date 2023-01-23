71c060a9b27d8e05b4e69297b3a2715a827f2b45
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-23 07:19:34 +0530

    feat(web/metrics): implement loading ui

--- apps/web/src/components/metrics/index.tsx
+++ apps/web/src/components/metrics/index.tsx
@@ -28,3 +28,56 @@ export default function Metrics({ data }: Props) {
     </div>
   );
 }
+
+export function MetricsShimmer() {
+  const metrics = [
+    {
+      label: "Pages",
+      tableHeader: ["Page", "Views"],
+    },
+    {
+      label: "UTM",
+      tableHeader: ["Source", "Views"],
+    },
+    {
+      label: "Countries",
+      tableHeader: ["Country", "Views"],
+    },
+    {
+      label: "Browsers",
+      tableHeader: ["Browser", "Views"],
+    },
+    {
+      label: "Devices",
+      tableHeader: ["Device", "Views"],
+    },
+    {
+      label: "Events",
+      tableHeader: ["Event", "Views"],
+    },
+  ] as const;
+
+  return (
+    <div className="wrap grid auto-rows-auto grid-cols-1 gap-4 md:grid-cols-2">
+      {metrics.map(({ label, tableHeader: [title, dataValueType] }) => (
+        <div
+          className="flex h-full w-full flex-col gap-4 rounded-xl border border-gray-200 px-6 pb-8 pt-5 dark:border-gray-800"
+          key={label}
+        >
+          <div className="flex w-full items-center">
+            <h2 className="text-lg font-semibold md:text-xl">{label}</h2>
+          </div>
+          <table className="flex h-full w-full flex-col gap-4">
+            <thead>
+              <tr className="flex justify-between text-gray-400 dark:text-gray-600">
+                <th className="font-medium">{title.toUpperCase()}</th>
+                <th className="font-medium">{dataValueType.toUpperCase()}</th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </div>
+      ))}
+    </div>
+  );
+}
