1b7d378e76b4a4b418336333032137229b8451be
Vedant Nandwana <70624701+vedantnn71@users.noreply.github.com>
2023-01-25 11:39:03 +0530

    feat(web/websiteCard): show insightful message  (#50)
    * When the site has no views -> Got no views since the past day
    
    * feat(web/app): pass down createdAt to website card

--- apps/web/src/components/websiteCard/index.tsx
+++ apps/web/src/components/websiteCard/index.tsx
@@ -5,22 +5,44 @@ interface Props {
   url: string;
   name: string;
   views: number;
+  createdAt: number;
 }
 
 export function WebsiteCardShimmer() {
   return (
     <div className="group flex flex-col items-start justify-start gap-4">
-      <div className="relative grid h-40 w-full place-items-center overflow-hidden rounded-2xl bg-gray-100 dark:bg-gray-800 animate-pulse" />
+      <div className="relative grid h-40 w-full animate-pulse place-items-center overflow-hidden rounded-2xl bg-gray-100 dark:bg-gray-800" />
 
-      <div className="flex w-full justify-between animate-pulse flex-col gap-2">
-        <div className="bg-gray-300 dark:bg-gray-700 h-4 w-20"/>
-        <div className="text-gray-300 dark:bg-gray-700 w-28 h-3" />
+      <div className="flex w-full animate-pulse flex-col justify-between gap-2">
+        <div className="h-4 w-20 bg-gray-300 dark:bg-gray-700" />
+        <div className="h-3 w-28 text-gray-300 dark:bg-gray-700" />
       </div>
     </div>
   );
 }
 
-export default function WebsiteCard({ url, name, views }: Props) {
+export default function WebsiteCard({ url, name, views, createdAt }: Props) {
+  const ONE_DAY = 1000 * 60 * 60 * 24;
+  const recentlyCreated = createdAt > Date.now() - ONE_DAY * 2;
+  let insights: string;
+
+  switch (views) {
+    case 0:
+      insights = "Got no views since the past day";
+      break;
+
+    case 1:
+      insights = "Got 1 view since the past day";
+      break;
+
+    default:
+      insights = `Got ${views} views since the past day`;
+  }
+
+  if (recentlyCreated) {
+    insights = "Just recently created";
+  }
+
   return (
     <div className="group flex flex-col items-start justify-start gap-4">
       <Link
@@ -46,9 +68,7 @@ export default function WebsiteCard({ url, name, views }: Props) {
           <h1 className="text-lg font-medium text-gray-700 dark:text-gray-300">
             {name}
           </h1>
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            Got {views} views since the past day
-          </p>
+          <p className="text-sm text-gray-500 dark:text-gray-400">{insights}</p>
         </div>
 
         <CardDropdown name={name} id={url} />
