e5631ba93d41d70ad6a253226141970050f28324
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-01-26 11:12:09 +0530

    feat(web/analytics-sidebar): get data from website store

--- apps/web/src/components/analyticsSidebar/index.tsx
+++ apps/web/src/components/analyticsSidebar/index.tsx
@@ -4,28 +4,27 @@ import DateSelect from "~/components/dateSelect";
 import Link from "next/link";
 import SignInLinkButton from "./signInLinkButton";
 
+import useWebsiteStore from "~/store/website";
+
 interface Props {
-  url: string;
-  name: string;
   isAuthed: boolean;
 }
 
-export default function AnalyticsSidebar({
-  name,
-  url,
-  isAuthed,
-}: Props) {
+export default function AnalyticsSidebar({ isAuthed }: Props) {
+  const url = useWebsiteStore.getState().url;
+  const name = useWebsiteStore.getState().name;
+
   return (
     <>
       <div className="flex gap-4 overflow-hidden lg:hidden">
-        <img src={`/favicon/${url}`} alt={name} width={28} height={28}  />
+        <img src={`/favicon/${url}`} alt={name} width={28} height={28} />
 
         <h2 className="truncate text-xl font-bold text-gray-800 dark:text-gray-100">
           {name}
         </h2>
       </div>
 
-      <aside className="pr-6 hidden fixed h-screen w-64 flex-col gap-4 bg-gray-100 pb-12 dark:bg-gray-900 lg:flex">
+      <aside className="fixed hidden h-screen w-64 flex-col gap-4 bg-gray-100 pr-6 pb-12 dark:bg-gray-900 lg:flex">
         <div className="flex gap-4">
           <img
             src={`/favicon/${url}`}
@@ -36,7 +35,7 @@ export default function AnalyticsSidebar({
           />
 
           <div className="flex flex-col overflow-hidden">
-            <h2 className="truncate text-xl font-bold text-gray-800 dark:text-gray-100 -mt-1">
+            <h2 className="-mt-1 truncate text-xl font-bold text-gray-800 dark:text-gray-100">
               {name}
             </h2>
             <p className="text-sm text-gray-500 dark:text-gray-400">{url}</p>
