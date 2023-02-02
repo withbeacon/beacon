030186a42cb3fdf2650c293aa59f4eda5f129d25
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-02-02 16:22:21 +0530

    fix(web/bottomNav): light background in dark mode

--- apps/web/src/components/bottomNav/index.tsx
+++ apps/web/src/components/bottomNav/index.tsx
@@ -37,11 +37,14 @@ export default function BottomNav() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-center gap-6 border-t border-gray-200 bg-white py-4 shadow-lg shadow-gray-900/10 dark:border-gray-800 dark:shadow-gray-900/10 lg:hidden z-40">
+    <nav className="fixed bottom-0 left-0 right-0 z-40 flex w-full items-center justify-center gap-6 border-t border-gray-200 bg-white py-4 shadow-lg shadow-gray-900/10 dark:border-gray-800 dark:bg-gray-900 dark:shadow-gray-900/10 lg:hidden">
       <NavItem href="/">
         <HomeIcon />
       </NavItem>
-      <button className="flex flex-col items-center justify-center" onClick={() => setIsDialogOpen(true)}>
+      <button
+        className="flex flex-col items-center justify-center"
+        onClick={() => setIsDialogOpen(true)}
+      >
         <PlusIcon />
       </button>
       <NavItem href="/settings">
@@ -55,17 +58,16 @@ export default function BottomNav() {
 
 export function BottomNavShimmer() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-center gap-6 border-t border-gray-200 bg-gray-900 py-4 shadow-lg shadow-gray-900/10 dark:border-gray-800 dark:shadow-gray-900/10 lg:hidden z-40">
+    <nav className="fixed bottom-0 left-0 right-0 z-40 flex w-full items-center justify-center gap-6 border-t border-gray-200 bg-gray-900 py-4 shadow-lg shadow-gray-900/10 dark:border-gray-800 dark:shadow-gray-900/10 lg:hidden">
       <div className="flex flex-col items-center justify-center">
-        <div className="w-7 h-7 rounded-full bg-gray-400 dark:bg-gray-600 animate-pulse" />
+        <div className="h-7 w-7 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600" />
       </div>
       <div className="flex flex-col items-center justify-center">
-        <div className="w-7 h-7 rounded-full bg-gray-400 dark:bg-gray-600 animate-pulse" />
+        <div className="h-7 w-7 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600" />
       </div>
       <div className="flex flex-col items-center justify-center">
-        <div className="w-7 h-7 rounded-full bg-gray-400 dark:bg-gray-600 animate-pulse" />
+        <div className="h-7 w-7 animate-pulse rounded-full bg-gray-400 dark:bg-gray-600" />
       </div>
     </nav>
   );
 }
-
