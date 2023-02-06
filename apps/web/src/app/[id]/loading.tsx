f26735e8406efe5b9b70154d81d7bb8fa6f17aac
Vedant Nandwana <70624701+vedantnn71@users.noreply.github.com>
2023-02-06 20:51:24 +0530

    feat(web/analytics-sidebar): implement loading component (#57)
    implement shimmer loading ui for AnalyticsSidebar

--- apps/web/src/app/[id]/loading.tsx
+++ apps/web/src/app/[id]/loading.tsx
@@ -1,11 +1,11 @@
-import { SidebarShimmer } from "~/components/sidebar";
+import { AnalyticsSidebarShimmer } from "~/components/analyticsSidebar";
 import { InsightsShimmer } from "~/components/insights";
 import { MetricsShimmer } from "~/components/metrics";
 
 export default function Loading() {
   return (
     <div className="flex flex-col gap-6 m-4">
-      <SidebarShimmer />
+      <AnalyticsSidebarShimmer />
 
       <div className="ml-0 flex w-full flex-col gap-6 overflow-scroll lg:ml-64">
         <div className="hide-scrollbar flex gap-4 overflow-scroll lg:overflow-hidden">
--- apps/web/src/components/analyticsSidebar/index.tsx
+++ apps/web/src/components/analyticsSidebar/index.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
-import { HomeIcon, FileIcon } from "@beacon/ui";
+import { HomeIcon, FileIcon, CalendarIcon } from "@beacon/ui";
 import DateSelect from "~/components/dateSelect";
 import Link from "next/link";
 import SignInLinkButton from "./signInLinkButton";
@@ -81,5 +81,35 @@ function SidebarLink({ href, children }: SidebarLinkProps) {
     <Link href={href} className="flex gap-2 text-gray-700 dark:text-gray-300">
       {children}
     </Link>
+  );
+}
+export function AnalyticsSidebarShimmer() {
+  return (
+    <aside className="fixed hidden h-screen w-64 flex-col gap-4 bg-gray-100 pr-6 pb-12 dark:bg-gray-900 lg:flex">
+      <div className="flex items-center gap-3">
+        <div className="h-7 w-7 rounded-full bg-gray-300 dark:bg-gray-700" />
+
+        <h2 className="truncate text-xl font-bold text-gray-800 dark:text-gray-100">
+          Hang on...
+        </h2>
+      </div>
+
+      <div className="flex w-full items-center justify-center gap-2 rounded-lg border border-gray-300 py-2.5 dark:border-gray-700">
+        <CalendarIcon />
+        Past Week
+      </div>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <SidebarLink href="/">
+          <HomeIcon />
+          <span>Dashboard</span>
+        </SidebarLink>
+
+        <SidebarLink href="#">
+          <FileIcon />
+          <span>Documentation</span>
+        </SidebarLink>
+      </div>
+    </aside>
   );
 }
