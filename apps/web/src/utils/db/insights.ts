5f4e9ef6ea5b4dda83e046076867abfbbc674b67
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-02-07 16:18:22 +0530

    feat(utils/insights): return percentage difference in comparison to previous period

--- apps/web/src/utils/db/insights.ts
+++ apps/web/src/utils/db/insights.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@beacon/db";
+import { differencePercentage } from "@beacon/basics";
 
 interface Params {
   id: string;
@@ -10,6 +11,10 @@ export interface Insights {
   pageViews: number;
   sessions: number;
   avgDuration: number;
+  growth: {
+    pageViews: number;
+    sessions: number;
+  };
 }
 
 export async function getInsights({ id, from, to }: Params): Promise<Insights> {
@@ -58,9 +63,46 @@ export async function getInsights({ id, from, to }: Params): Promise<Insights> {
     },
   });
 
+  const previousPeriod = {
+    from: new Date(from.getTime() - (to.getTime() - from.getTime())),
+    to: from,
+  };
+
+  const previousSessions = await prisma.userSession.count({
+    where: {
+      website: {
+        id,
+      },
+
+      createdAt: {
+        gte: previousPeriod.from,
+        lte: previousPeriod.to,
+      },
+    },
+  });
+
+  const previousPageViews = await prisma.pageView.count({
+    where: {
+      website: {
+        id,
+      },
+
+      createdAt: {
+        gte: previousPeriod.from,
+        lte: previousPeriod.to,
+      },
+    },
+  });
+
+  const growth = {
+    pageViews: differencePercentage(pageViews, previousPageViews),
+    sessions: differencePercentage(sessions, previousSessions),
+  };
+
   return {
     pageViews,
     sessions,
+    growth,
     avgDuration: avgDuration ?? 0,
   };
 }
