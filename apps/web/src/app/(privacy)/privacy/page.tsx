5f1fa4ade26a6bb9ab682e8d319b720de8f0457d
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-02-22 08:49:35 +0530

    fix(app/privacy): return null

--- apps/web/src/app/(privacy)/privacy/page.tsx
+++ apps/web/src/app/(privacy)/privacy/page.tsx
@@ -2,5 +2,6 @@ import { redirect } from "next/navigation";
 
 export default function Privacy() {
   redirect("https://github.com/withbeacon/beacon/blob/main/PRIVACY.md");
+  return null;
 }
 
--- apps/web/src/app/(privacy)/tos/page.tsx
+++ apps/web/src/app/(privacy)/tos/page.tsx
@@ -2,5 +2,6 @@ import { redirect } from "next/navigation";
 
 export default function TermOfService() {
   redirect("https://github.com/withbeacon/beacon/blob/main/TOS.md");
+  return null;
 }
 
