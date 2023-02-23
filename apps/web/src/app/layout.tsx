f99b2c63cf4bd9d988db21a73c7893cc0162c8c7
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-02-23 18:17:02 +0530

    feat(web/components): implement command palette

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,17 +1,38 @@
 import type { PropsWithChildren } from "react";
 import { ServerThemeProvider } from "@wits/next-themes";
 import { Inter } from "@next/font/google";
+import CommandPalette from "~/components/commandPalette";
+
+import { prisma } from "@beacon/db";
+import { getServerSession } from "@beacon/auth";
 
 import "~/styles/globals.css";
 
 const font = Inter({ subsets: ["latin"] });
 
 export default async function RootLayout({ children }: PropsWithChildren) {
+  const session = await getServerSession();
+  const websites = await prisma.website.findMany({
+    where: {
+      user: {
+        email: session?.user?.email || undefined,
+      },
+    },
+    select: {
+      id: true,
+      url: true,
+      name: true,
+    },
+  });
+
   return (
     <ServerThemeProvider attribute="class">
       <html lang="en" className={font.className}>
         <head />
-        <body>{children}</body>
+        <body>
+          <CommandPalette websites={websites} />
+          {children}
+        </body>
       </html>
     </ServerThemeProvider>
   );
