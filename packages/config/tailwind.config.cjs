f44f5733f1ee8550c75036741bd3cd7a8be35880
Vedant Nandwana <70624701+vedantnn71@users.noreply.github.com>
2023-02-23 19:06:23 +0530

    feat(config/tailwind): add animations (#69)

--- packages/config/tailwind.config.cjs
+++ packages/config/tailwind.config.cjs
@@ -43,10 +43,25 @@ module.exports = {
       dropShadow: {
         "insight-card": "2px 2px 48px rgba(194, 194, 194, 0.00)",
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [
     require("tailwindcss-radix"),
+    require("tailwindcss-animate"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
     require("@headlessui/tailwindcss"),
