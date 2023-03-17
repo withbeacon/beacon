62325d101813a0728c532e2a038348a4c797d893
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-03-17 10:13:41 +0530

    feat(web/landing): redirect to /dashboard if already logged in

--- apps/web/src/app/(marketing)/page.tsx
+++ apps/web/src/app/(marketing)/page.tsx
@@ -1,18 +1,19 @@
 import Nav from "./components/nav";
 import Hero from "./components/hero";
 import Features from "./components/features";
-
-import { getServerSession } from "@beacon/auth";
-import { redirect } from "next/navigation";
 import WindowImage from "./components/hero/windowImage";
 import Footer from "./components/footer";
 
+import { getServerSession } from "@beacon/auth";
+import { redirect } from "next/navigation";
+
 export default async function Page() {
   const session = await getServerSession();
-  //if (session) {
-  //  redirect("/dashboard");
-  //  return null;
-  //}
+
+  if (session) {
+    redirect("/dashboard");
+    return null;
+  }
 
   return (
     <div className="bg-white selection:bg-primary-100 overflow-hidden">
