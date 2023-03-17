d9861b1769ac060cf1fe04901fffb4759b5ca19e
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-03-17 11:35:07 +0530

    feat(web/auth): deny access if not approved in waitlist

--- apps/web/src/utils/auth.ts
+++ apps/web/src/utils/auth.ts
@@ -1,11 +1,21 @@
 import { getServerSession } from "@beacon/auth";
 import { redirect } from "next/navigation";
+import { prisma } from "@beacon/db";
 
 export async function protect() {
   const session = await getServerSession();
 
   if (!session) {
     redirect("/sign-in");
+  }
+
+  const waitlist = await prisma.waitlist.findUnique({
+    where: { email: session.user?.email || undefined },
+    select: { isApproved: true },
+  });
+
+  if (!waitlist?.isApproved) {
+    redirect("/sign-in?error=access_denied");
   }
 }
 
