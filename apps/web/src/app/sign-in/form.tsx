5ccb5466ccb95e2298c0071ffb79d8ece7361a30
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-03-24 21:55:22 +0530

    feat(web/sign-in): implement magic link sign in

---
+++ apps/web/src/app/sign-in/form.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Label, Button } from "@beacon/ui";
+
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { cx } from "class-variance-authority";
+import { signIn } from "next-auth/react";
+
+type FormValues = {
+  email: string;
+};
+
+export default function SignInForm() {
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isMutating = isFetching || isPending;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormValues>();
+
+  async function onSubmit({ email }: FormValues) {
+    setIsFetching(true);
+    await signIn("email", { email });
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex w-full flex-col gap-3"
+    >
+      <div className="mt-2 flex w-full flex-col gap-2">
+        <Label error={errors.email?.message} htmlFor="email">
+          Email
+        </Label>
+        <input
+          className={cx(
+            "w-full rounded-xl border border-gray-200 bg-white py-2.5 px-3 transition-all duration-100 dark:border-gray-700 dark:bg-gray-800",
+            errors.email && "!ring-red-500 focus:!border-red-500"
+          )}
+          placeholder="hello@world.com"
+          type="email"
+          id="email"
+          disabled={!!errors.email || isMutating}
+          required
+          {...register("email", { required: true })}
+        />
+      </div>
+      <Button
+        variant="primary"
+        size="lg"
+        className="my-2"
+        loading={isSubmitting}
+        fullWidth
+      >
+        Sign In
+      </Button>
+    </form>
+  );
+}
--- apps/web/src/app/sign-in/page.tsx
+++ apps/web/src/app/sign-in/page.tsx
@@ -1,12 +1,13 @@
-import { Logo, ErrorIcon } from "@beacon/ui";
+import { Logo, ErrorIcon, Label, Button } from "@beacon/ui";
 import Link from "next/link";
-import SignInButton from "~/components/signInButton";
+import GoogleOAuthSignInButton from "~/components/googleOAuthSignInButton";
+import SignInForm from "./form";
 
 type Props = {
   searchParams?: { [key: string]: string | string[] | undefined };
 };
 
-export default async function SignIn({ searchParams }: Props) {
+export default function SignIn({ searchParams }: Props) {
   const error = searchParams?.error as string | undefined;
 
   return (
@@ -15,7 +16,6 @@ export default async function SignIn({ searchParams }: Props) {
         <Logo className="mr-2 h-8 w-8" />
         <h2 className="text-2xl font-bold md:text-3xl">Beacon</h2>
       </div>
-
       <div className="my-auto flex max-w-lg flex-col items-start gap-3 px-6 md:p-0">
         <h1 className="text-2xl font-bold text-gray-900 dark:text-gray-100 md:text-3xl">
           Sign in to Beacon
@@ -25,7 +25,6 @@ export default async function SignIn({ searchParams }: Props) {
           Create your account for Beacon. The analytics tool which is just
           created for you in mind.
         </p>
-
         {error === "access_denied" ? (
           <div className="my-2 flex select-none flex-col gap-2 rounded-xl border border-gray-200 bg-gray-100 py-4 px-6 text-gray-800 hover:bg-gray-200 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700">
             <div className="flex items-center gap-2">
@@ -36,13 +35,21 @@ export default async function SignIn({ searchParams }: Props) {
             </div>
             <p className="text-base text-gray-600 dark:text-gray-400">
               We are currently in early access, if you want to get access to
-              Beacon, please sign up for the <Link href="/" className="underline underline-offset-4">waitlist</Link>. We
-              will send you an invite asap.
+              Beacon, please sign up for the{" "}
+              <Link href="/" className="underline underline-offset-4">
+                waitlist
+              </Link>
+              . We will send you an invite asap.
             </p>
           </div>
         ) : null}
-
-        <SignInButton />
+        <SignInForm />
+        <div className="flex w-full items-center gap-2">
+          <div className="h-px w-full bg-gray-200 dark:bg-gray-700" />
+          <span className="text-gray-500 dark:text-gray-400">OR</span>
+          <div className="h-px w-full bg-gray-200 dark:bg-gray-700" />
+        </div>
+        <GoogleOAuthSignInButton />
         <p className="text-base text-gray-500 dark:text-gray-400 md:text-lg">
           By signing in to Beacon, you agree to our terms of service and privacy
           policy.
@@ -53,5 +60,5 @@ export default async function SignIn({ searchParams }: Props) {
 }
 
 export const metadata = {
-  title: "Sign in to Beacon",
+ title: "Sign in to Beacon",
 };
