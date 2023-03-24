e220ff526dfa3ad4782a92da54cd30770d89d212
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-03-24 22:32:43 +0530

    fix(auth/email): large margins

--- packages/auth/src/email.tsx
+++ packages/auth/src/email.tsx
@@ -97,7 +97,7 @@ const container = {
 const heading = {
   fontSize: "28px",
   fontWeight: "bold",
-  marginTop: "48px",
+  marginTop: "20px",
 };
 
 const body = {
@@ -106,7 +106,7 @@ const body = {
 
 const paragraph = {
   fontSize: "16px",
-  lineHeight: "26px",
+  lineHeight: "2px",
 };
 
 const link = {
