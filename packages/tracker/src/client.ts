5c3efecd5458d0912de2b979254ef465959032d7
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-03-25 22:05:38 +0530

    fix(tracker): don't track page if not asked

--- packages/tracker/src/client.ts
+++ packages/tracker/src/client.ts
@@ -6,6 +6,7 @@ export const isDoNotTrackEnabled = doNotTrack === '1';
 export const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 export const { currentScript: script } = document;
+export const track404 = script.getAttribute("data-track-404") === "true";
 
 if (!script) {
   throw new Error("[beacon] unable to get the script for beacon analytics")
