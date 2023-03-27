1187c9faa052bbc3438d89a042cb6724a1ada4a4
Vedant Nandwana <vedantnn7.git@gmail.com>
2023-03-27 12:35:58 +0530

    fix(web/source-metrics): invalid default utm param

--- apps/web/src/components/metrics/source.tsx
+++ apps/web/src/components/metrics/source.tsx
@@ -10,7 +10,7 @@ interface Props {
 }
 
 export function SourceMetrics({ data }: Props) {
-  const [utmParam, setUtmParam] = useState("ref");
+  const [utmParam, setUtmParam] = useState("referrer");
   const isActive = (val: string): boolean => utmParam === val;
 
   const options = [
